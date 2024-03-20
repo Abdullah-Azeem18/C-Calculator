36f874d100e0f63f027314bb1c935218f37d7a8b
Abdullah Azeem <163125247+Abdullah-Azeem18@users.noreply.github.com>
2024-03-20 12:34:41 +0500

    Add files via upload

---
+++ C++ calculator.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    
+char calculator;
+float a,b,Ans;
+cout<<"Enter First Value A =";
+cin>>a;
+cout<<"Enter First Value B =";
+cin>>b;
+cout<<"Enter Which operation you peform +,-,*,/  "<<"\n";
+cin>>calculator;
+
+
+switch (calculator)
+{
+    case '+':
+      Ans=a+b;
+      cout<<Ans;
+      break;
+      
+      case '-':
+      Ans=a-b;
+      cout<<Ans;
+      break;
+      
+      case '*':
+      Ans=a*b;
+      cout<<Ans;
+      break;
+      
+      case '/':
+      Ans=a/b;
+      cout<<Ans;
+      break;
+      
+      default:
+      cout<<"Unknown operator";
+      break;
+      
+     }
+     
+     
+    return 0;
+}
